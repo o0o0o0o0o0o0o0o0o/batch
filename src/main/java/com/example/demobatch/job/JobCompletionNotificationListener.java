b4ae9894bb34abd81be3a3b32aa8614bf8dec162
王志强 <zhiqiang.wang@markenin.cn>
2018-11-09 19:32:00 +0800

    添加依赖

---
+++ src/main/java/com/example/demobatch/job/JobCompletionNotificationListener.java
@@ -0,0 +1,42 @@
+package com.example.demobatch.job;
+
+import com.example.demobatch.bean.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * TODO 描述:
+ *
+ * @author zhiqiang.wang@marketin.cn
+ * @create 2018-11-09 下午7:21
+ * @since dev-1.9.2
+ */
+public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
+
+    private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            log.info("!!! JOB FINISHED! Time to verify the results");
+
+            jdbcTemplate.query("SELECT first_name, last_name FROM t_person",
+                    (rs, row) -> new Person(
+                            rs.getString(1),
+                            rs.getString(2))
+            ).forEach(person -> log.info("Found <" + person + "> in the database."));
+        }
+    }
+}
